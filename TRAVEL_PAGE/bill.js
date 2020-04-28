f2e8e2e048b3d63ba72b23f9fa8db83f93644dd6
surajchapagain20 <64243291+surajchapagain20@users.noreply.github.com>
2020-04-28 16:23:27 +0545

    Add files via upload

---
+++ TRAVEL_PAGE/bill.js
@@ -0,0 +1,15 @@
+
+function Calculate() {
+    var COST_AMT = parseInt(num1 = document.getElementById("cost").value); + "<br>";
+    var QTTY = parseInt(num1 = document.getElementById("qty").value); + "<br>";
+    var Paid_amt = parseInt(num1 = document.getElementById("pAmount").value); + "<br>";
+    
+    var PRICE = (COST_AMT * QTTY);
+    var dBalance =  PRICE - Paid_amt;
+    var tax =  PRICE * 13 / 100;
+  
+
+    document.getElementById("Total_Amount").innerHTML = 'Total Amount NPR: ' + (PRICE);
+    document.getElementById("DueAmount").innerHTML = 'Due Balance: ' + (dBalance + tax);
+    document.getElementById("TAX_AMT").innerHTML = 'VAT: ' + (tax);
+}
